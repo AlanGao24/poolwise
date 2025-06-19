3d6bfe8478e57c173481198c5ea7736a780c32fe
AlanGao <alangao0924@gmail.com>
2025-06-19 05:40:49 +0000

    AboutUsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
 import HeroSection from '@/component/HeroSection';
+import Image from 'next/image';
 // import BottomSection from '@/component/BottomSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <div className = "w-full h-screen bg-amber-50">
-      </div>
+      <section className='w-full h-screen bg-amber-50 flex items-center justify-center'>
+          <div className='max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 gap-32 items-center'>
+            <div className='space-y-6'>
+              <h2 className="text-3xl font-bold text-gray-800">About Us</h2>
+              <p className='text-gray-700 text-lg leading-relaxed'>
+                Poolwise Living is a proudly Western Australian owned and operated family business with over 40 years of experience. We’re committed to providing genuine, honest, and obligation-free advice, along with exceptional customer service.</p>
+              <p className='text-gray-700 text-lg leading-relaxed'>
+                Our dedicated Service Division, led by Jodie, ensures smooth day-to-day operations. Our experienced technicians service the entire Perth metro area and beyond, all year round. We understand that pool systems and equipment can be complex, so we take the time to clearly explain how everything works and why proper maintenance and chemicals are essential. </p>
+              <p className='text-gray-700 text-lg leading-relaxed'>
+                We are proud members of SPASA (Swimming Pool & Spa Association) and our technicians are trained through SPASA, the Royal Life Saving Society, and in-house programs delivered by leading suppliers and manufacturers.</p>
+            </div>
+            <div className='w-full h-full'>
+              <Image 
+                src="/AboutUs.webp"
+                alt="Description of image"
+                // layout="responsive"
+                width={1800}
+                height={1500}
+                className="rounded-xl shadow-md object-cover"
+              />
+            </div>
+          </div>
+
+      </section>
     </main>
   );
 }
--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -5,21 +5,27 @@ import Link from "next/link";
 export default function HeroSection(){
     return(
         <section
-        // className="flex items-center justify-center h-screen text-white bg-center bg-cover" 
-        // className="fixed inset-0 flex items-center justify-center h-screen text-white bg-center bg-cover"
         className="top-0 flex items-center justify-center h-screen text-white bg-fixed bg-center bg-cover"
         style={{ backgroundImage: "url('/waterBackground.png')" }}>
-            <div className="px-4 text-center">
-                <h1 className="text-4xl font-bold md:text-8xl drop-shadow-lg">
+            <div className="flex flex-col px-4 text-center -mt-15 gap-y-5">
+                <h1 className="text-6xl font-bold md:text-9xl drop-shadow-lg/60">
                     Poolwise
                 </h1>
-                <p className="mt-4 text-lg md:text-3xl drop-shadow-sm">
+                <p className="text-2xl mt-15 md:text-3xl drop-shadow-sm/30">
                     Professional swimming pool maintenance solutions
                 </p>
-                <div className="flex flex-col justify-center gap-4 mt-6 md:flex-row">
+                <p className="text-2xl md:px-28 md:text-3xl drop-shadow-sm/30">
+                    We are a team of pool maintenance experts dedicated to keeping your pool clean, safe, and enjoyable all year round.
+                </p>
+                <div className="flex justify-center gap-4 mt-8 md:flex-row">
                     <Link
                     href="/contact"
-                    className="px-6 py-3 font-semibold text-black transition bg-yellow-400 rounded-full shadow hover:bg-yellow-300">
+                    className="
+                        px-10 py-4 text-sm            // 手机端：内边距小，文字小
+                        md:px-25 md:py-5 md:text-2xl // 中屏及以上：内边距变大，字体变正常
+                        font-semibold text-black transition bg-yellow-400 rounded-full shadow hover:bg-yellow-300
+                        hover:shadow-lg hover:scale-115
+                        ">
                         Booking Now
                     </Link>
                 </div>
